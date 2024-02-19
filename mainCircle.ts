247c2a4716ebc88d9653c93526333d7cd8f82209
Huy Tran <huytran21007@gmail.com>
2024-02-20 06:51:16 +0700

    Update print message for Circle

--- mainCircle.ts
+++ mainCircle.ts
@@ -4,14 +4,19 @@ import Point2D from "./Point2D";
 const mainCircle = () => {
   // test circle
   let point = new Point2D();
+  console.log("Update point");
   point.scan();
+  console.log("After update, point info is: ");
+  point.print();
   let newCircle = new Circle(point, 10);
+  console.log("Created circle is: ");
   console.log(newCircle);
-  point.x = 5;
-  point.y = 100;
 
   // test scan circle
+  console.log("Update circle");
   newCircle.scan();
+
+  console.log("Circle info: ");
   // test newCircle
   newCircle.print();
 };
